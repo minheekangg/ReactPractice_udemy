2389c095ed4b4d0426df652f2358259fde0593cd
minheekangg <minheekangg@gmail.com>
2019-01-30 09:23:40 -0500

    search field

---
+++ src/component/app.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import SearchBar from './searchBar';
+const App = () => {
+    return <div><SearchBar /></div>
+}
+
+export default App
---
+++ src/component/searchBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SearchBar = () => {
+    return (<div className="ui segment">
+        <form className="ui form">
+            <div className="field">
+                <input type="text"/>
+            </div>
+        </form>
+    </div>)
+}
+
+export default SearchBar
