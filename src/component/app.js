d6a6628666cb1970e6fa85b09e830e056eed1512
minheekangg <minheekangg@gmail.com>
2019-01-30 10:16:06 -0500

    api added

--- src/component/app.js
+++ src/component/app.js
@@ -1,7 +1,30 @@
 import React from 'react'
+import axios from 'axios';
 import SearchBar from './searchBar';
-const App = () => {
-    return <div><SearchBar /></div>
+
+const KEY = process.env.REACT_APP_API_KEY;
+class App extends React.Component {
+  
+    onSearchedSubmit = searchedInput => {
+        console.log(KEY);
+        axios
+          .get("https://api.unsplash.com/search/photos", {
+            params: { query: searchedInput },
+            headers: {
+              Authorization:
+                `Client-ID `+ process.env.REACT_APP_API_KEY
+            }
+          })
+            .then(r => console.log(`Client-ID ` + process.env.REACT_APP_API_KEY));
+    }
+
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: "10px" }}>
+        <SearchBar onSearchedSubmit={this.onSearchedSubmit} />
+      </div>
+    );
+  }
 }
 
 export default App
--- src/component/searchBar.js
+++ src/component/searchBar.js
@@ -1,13 +1,31 @@
 import React from 'react';
 
-const SearchBar = () => {
-    return (<div className="ui segment">
-        <form className="ui form">
-            <div className="field">
-                <input type="text"/>
-            </div>
+class SearchBar extends React.Component {
+  state = {
+    searched: ""
+  };
+
+  //   NOT SEARCHBAR JOB TO KNOW WHAT TO DO WITH SEARCHED TERM
+  onFormSubmit = e => {
+    e.preventDefault();
+    this.props.onSearchedSubmit(this.state.searched)
+  };
+
+  render() {
+    return (
+      <div className="ui segment">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
+          <div className="field">
+            <label>Image Search</label>
+            <input
+              type="text"
+              onChange={e => this.setState({ searched: e.target.value })}
+            />
+          </div>
         </form>
-    </div>)
+      </div>
+    );
+  }
 }
 
 export default SearchBar
